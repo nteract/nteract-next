58970a9b660070e6775186821740ba42901cb65d
Kyle Kelley <rgbkrk@gmail.com>
2024-02-02 15:40:57 -0800

    hooray

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import Cell from "@/components/Cell";
-
-import { Button} from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import { useNotebook } from '@/hooks/useNotebook';
+
+import { MarkdownCell } from "@/components/markdown-cell"
+import { CodeCell } from '@/components/code-cell'
 
 function App() {
   const { cells, createCodeCell, createMarkdownCell } = useNotebook();
   return (
     <div>
-      {cells.map((cellId: string) => (
-          <Cell cellId={cellId} key={cellId}/>
-      ))}
+      {cells.map((cellInfo) => {
+        switch (cellInfo.cellType) {
+          case "code":
+            return <CodeCell cellId={cellInfo.id} key={cellInfo.id} />
+          case "markdown":
+            return <MarkdownCell cellId={cellInfo.id} key={cellInfo.id} />
+          default:
+            return "Unknown cell type"
+        }
+      })}
       <Button onClick={createCodeCell}>New Code Cell</Button>
       <Button onClick={createMarkdownCell}>New Markdown Cell</Button>
     </div>
--- src/hooks/useNotebook.ts
+++ src/hooks/useNotebook.ts
@@ -1,13 +1,18 @@
 import { useState, useCallback } from 'react';
 import { invoke } from "@tauri-apps/api/tauri";
 
-export function useNotebook() {
-  const [cells, setCells] = useState<string[]>([]);
+type CellInfo = {
+  id: string,
+  cellType: "code" | "markdown"
+}
 
-  async function createCell(cellType: "code" | "markdown") {
+export function useNotebook() {
+  const [cells, setCells] = useState<CellInfo[]>([]);
+
+  const createCell = useCallback(async (cellType: "code" | "markdown") => {
     const id = (await invoke("create_cell", { cellType })) as string;
-    setCells(oldCells => [...oldCells, id]);
-  }
+    setCells(oldCells => [...oldCells, { id, cellType }]);
+  }, []);
 
   const createCodeCell = useCallback(() => createCell("code"), [createCell]);
   const createMarkdownCell = useCallback(() => createCell("markdown"), [createCell]);
